9a424b9472c93c29ef49243ae0636a8de5489def
Timothy Durbin <tdurbin@protonmail.com>
2020-11-05 20:45:25 -0800

    Cleaned up includes

--- network.h
+++ network.h
@@ -1,15 +1,9 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-
-#ifdef __WIN32__
-# include <winsock2.h>
-#else
-# include <sys/socket.h>
-#endif
-
 #include <iostream>
 #include <string>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
